391adbab02341e370142f2112d9bfbd1b247477e
joe95-ux <ogorktabi@gmail.com>
2023-07-26 23:51:30 +0100

    update linktree card component

--- components/LinkTreeCard.js
+++ components/LinkTreeCard.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import Link from 'next/link';
-
+import Link from 'next/link'
+import React from 'react'
 
 const LinkTreeCard = ({title, url, image}) => {
   return (
     <>
-    <span>
-        <Link className="" href={url}>
-            <img src={image} alt=""/>
-            <h4>{title}</h4>
-
-        </Link>
-    </span>
-
-
+        <span className='w-full'>
+            <Link className='flex flex-row items-center p-2 rounded-xl text-white bg-indigo-400 hover:bg-indigo-300 mb-3 mx-2 hover:translate-x-1 hover:translate-y-1 transition-all duration-500' target="_blank" href={`https://${url}`}>
+                <img className='bg-white rounded-md p-1 w-11 mr-5' src={image} alt="" />
+                <h4 className='md:text-lg'>{title}</h4>
+            </Link>
+        </span>
     </>
   )
 }
