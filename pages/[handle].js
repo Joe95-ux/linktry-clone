3563e6505ea9c388abdea2a46a16c94b430d8b9b
joe95-ux <ogorktabi@gmail.com>
2023-08-04 23:47:13 +0100

    add front end logic for user profile and links

--- pages/[handle].js
+++ pages/[handle].js
@@ -7,12 +7,21 @@ export const handle = () => {
   const router = useRouter();
   const [data, setData] = useState({});
   const [userFound, setUserFound] = useState(false);
+  const [social, setSocial] = useState({
+    facebook: '',
+    twitter: '',
+    instagram: '',
+    youtube: '',
+    linkedin: '',
+    github: ''
+  })
   useEffect(()=>{
     if(router.query?.handle){
       fetch(`http://localhost:8080/get/${router.query.handle}`).then(res=>res.json()).then(data=>{
         if(data.status === 'error') return toast.error(data.error);
         if(data.status === 'success'){
           setData(data.userData);
+          setSocial(data.socials);
           setUserFound(true);
 
         }
@@ -24,20 +33,22 @@ export const handle = () => {
     }
 
   }, [router.query])
+  if(!userFound){
+    return(
+        <div className='flex justify-center items-center h-screen'>
+            <div className="not-found px-3 ">
+                <h1 className='font-bold text-lg'>User Not found </h1>                    
+                <p>If you're looking for a page, double check the spelling.</p>
+                Create your own<Link className='bg-indigo-600 px-2 ml-2 text-white hover:bg-indigo-400 transition-all duration-500' href="/apply"> LinkTree</Link>
+            </div>
+        </div>
+    )
+  }
   return (
-    <>
-    {userFound ? 
     <div>
-      <Linktree data={data}/>
-    </div> : 
-    <div className='flex justify-center items-center h-screen'>
-      <div className="not-found px-3 ">
-        <h1 className='font-bold text-lg'>User Not found </h1>                    
-        <p>If you're looking for a page, double check the spelling.</p>
-        Create your own<Link className='bg-indigo-600 px-2 ml-2 text-white hover:bg-indigo-400 transition-all duration-500' href="/apply"> LinkTree</Link>
-      </div>
-    </div>}
-    </>
-    
+        <ShareButton/>
+        <Linktree data={data}/>
+        <SocialTree social={social} />
+    </div>
   )
 }
